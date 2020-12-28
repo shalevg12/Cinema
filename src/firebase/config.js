548c70f396d7a10ec41c24c0ccf4b45cbe6457af
shalevg12 <58697095+shalevg12@users.noreply.github.com>
2020-12-28 14:05:16 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBjcIyLaM4hA1_RnjQcViQhRhT-l6o70jk",
-    authDomain: "cinema-a78c0.firebaseapp.com",
-    projectId: "cinema-a78c0",
-    storageBucket: "cinema-a78c0.appspot.com",
-    messagingSenderId: "1096250389553",
-    appId: "1:1096250389553:web:2aaa81a2ba0eeb57269110"
+//Paste Your API Here
 };
